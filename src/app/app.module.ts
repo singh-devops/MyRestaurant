452eddbdf88fa43f217aa2478e1a5eac6d588960
Simranjeet <singh.hessen@gmail.com>
2021-10-01 21:53:21 +0530

    header and Navbar update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipelistComponent } from './recipe/recipelist/recipelist.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RecipeComponent,
+    RecipelistComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
