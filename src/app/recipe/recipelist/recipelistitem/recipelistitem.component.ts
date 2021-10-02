619faa7bf158033b8b6679022b713c0f823328d7
singh-devops <singh.hessen@gmail.com>
2021-10-02 14:24:26 +0530

    final commit for 2/oct

--- src/app/recipe/recipelist/recipelistitem/recipelistitem.component.ts
+++ src/app/recipe/recipelist/recipelistitem/recipelistitem.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipemodel } from 'src/app/recipemodel';
+
 @Component({
   selector: 'app-recipelistitem',
   templateUrl: './recipelistitem.component.html',
